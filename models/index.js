ce5a3c0810cd626a947afa87b9f57910ad2d3383
Andrew <afuselier23@gmail.com>
2018-07-30 21:15:29 -0700

    adding user model

---
+++ models/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  user: require('./userModel.js')
+}
---
+++ models/userModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: { 
+    type: String,
+    unique: true,
+    lowercase: true
+  },
+  password: String
+});
+
+module.exports = mongoose.model('user', userSchema);
